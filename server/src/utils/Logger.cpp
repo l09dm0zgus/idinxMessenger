23baa7c678617016eccdf9a704032d08734906fc
l09dm0zgus <cx9ps3@gmail.com>
2024-02-09 19:01:54 +0200

    Wrote class for initializing logger.

---
+++ server/src/utils/Logger.cpp
@@ -0,0 +1,40 @@
+//
+// Created by cx9ps3 on 09.02.2024.
+//
+
+#include "Logger.hpp"
+#include <filesystem>
+#include <boost/log/expressions.hpp>
+#include <boost/log/sinks/text_file_backend.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/utility/setup/console.hpp>
+#include <boost/log/sinks/text_ostream_backend.hpp>
+#include <boost/log/sources/logger.hpp>
+#include <boost/log/sinks/sync_frontend.hpp>
+
+void utils::Logger::initialize()
+{
+
+    if(!std::filesystem::exists("logs"))
+    {
+        std::filesystem::create_directory("logs");
+    }
+    logging::add_console_log(std::clog, keywords::format = FORMAT);
+    logging::add_file_log
+            (
+                    keywords::auto_flush = true,
+                    keywords::file_name = FILE_NAME ,
+                    keywords::rotation_size = 10 * 1024 * 1024,
+                    keywords::time_based_rotation = sinks::file::rotation_at_time_point(0, 0, 0),
+                    keywords::format = FORMAT
+            );
+
+    logging::core::get()->set_filter
+            (
+                    logging::trivial::severity >= logging::trivial::trace
+            );
+
+    utils::logging::add_common_attributes();
+}
+
---
+++ server/src/utils/Logger.hpp
@@ -0,0 +1,28 @@
+//
+// Created by cx9ps3 on 09.02.2024.
+//
+
+#pragma once
+#include <boost/log/trivial.hpp>
+#include <boost/assert.hpp>
+#include <sstream>
+
+namespace utils
+{
+    namespace logging = boost::log;
+    namespace src = boost::log::sources;
+    namespace sinks = boost::log::sinks;
+    namespace keywords = boost::log::keywords;
+
+
+    class Logger
+    {
+    private:
+      static constexpr std::string_view FILE_NAME = "logs/idinxServer_%N_%Y-%m-%d_%H-%M-%S.log";
+      static constexpr std::string_view FORMAT = "[%TimeStamp%] - PID = %ProcessID% - TID = %ThreadID% : [%Severity%] %Message%";
+    public:
+        static void initialize();
+    };
+
+#define ASSERT_LOG(expr, msg) if(!expr) { BOOST_LOG_TRIVIAL(fatal) <<"Assertion :" << #expr << " failed. Message: " << msg;  }
+}
