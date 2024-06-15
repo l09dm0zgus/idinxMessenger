4fc11ed8453fb89d802bc1be55b934f10cb6c5a9
cx9ps3 <cx9ps3@gmail.com>
2024-06-15 15:44:18 +0300

    Removed unused headers and connection to database.

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -4,20 +4,16 @@
 #include "config/ConfigManager.hpp"
 #include "config/ServerConfig.hpp"
 #include "config/DatabaseConfig.hpp"
-#include "database/Connection.hpp"
-#include <aes.h>
 
 int main()
 {
     try
     {
-        
         utils::Logger::initialize();
         config::ConfigManager::initialize();
         config::ConfigManager::addConfig<config::ServerConfig>();
         config::ConfigManager::addConfig<config::DatabaseConfig>();
         server::TCPServer tcpServer;
-        auto con = db::Connection::connect();
         tcpServer.run();
     }
     catch (std::exception &e)
