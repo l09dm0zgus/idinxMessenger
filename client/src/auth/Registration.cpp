ad0bbf79668abecdae5339f09e3dc7c3ff2d9728
cx9ps3 <cx9ps3@gmail.com>
2024-06-15 15:34:10 +0300

    Added class for Registration.

---
+++ client/src/auth/Registration.cpp
@@ -0,0 +1,35 @@
+//
+// Created by cx9ps3 on 6/14/24.
+//
+
+#include "Registration.hpp"
+#include "Connection.hpp"
+#include "boost/json.hpp"
+#include <sstream>
+
+auth::Registration::Registration(const std::shared_ptr<Connection> &newConnection) : connection(newConnection)
+{
+
+}
+
+std::string auth::Registration::registerNewAccount(const auth::Registration::AccountData &accountData)
+{
+    auto serializedAccountData = serializeAccountData(accountData);
+    connection->sendRequest<rest::Method::POST>(serializedAccountData,"/registration", connection->getIP(), "application/json");
+    auto response = connection->readResponse();
+    return boost::beast::buffers_to_string(response.body().data());
+}
+
+std::string auth::Registration::serializeAccountData(const auth::Registration::AccountData &accountData)
+{
+    boost::json::object obj;
+    obj["user_data"] =
+            {
+                    {"email",accountData.email},
+                    {"login",accountData.login},
+                    {"password",accountData.password}
+            };
+    std::stringstream ss;
+    ss << obj;
+    return ss.str();
+}
---
+++ client/src/auth/Registration.hpp
@@ -0,0 +1,30 @@
+//
+// Created by cx9ps3 on 6/14/24.
+//
+
+#pragma once
+#include <iostream>
+#include <memory>
+
+namespace auth
+{
+    class Connection;
+    class Registration
+    {
+    public:
+        struct AccountData
+        {
+            std::string login;
+            std::string password;
+            std::string email;
+        };
+    private:
+        std::shared_ptr<Connection> connection;
+        std::string serializeAccountData(const AccountData& accountData);
+
+    public:
+        explicit Registration(const std::shared_ptr<Connection>& newConnection);
+        std::string registerNewAccount(const AccountData& accountData);
+    };
+}
+
