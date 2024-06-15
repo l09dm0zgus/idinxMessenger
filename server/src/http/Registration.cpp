447c131082b87dfc4ed11aeca8ba5a8139c90a4a
cx9ps3 <cx9ps3@gmail.com>
2024-06-15 15:37:09 +0300

    Changing in handling request returning shared pointer to response.Added creating user account in server database.

--- server/src/http/Registration.cpp
+++ server/src/http/Registration.cpp
@@ -3,9 +3,12 @@
 //
 
 #include "Registration.hpp"
+#include "../database/Connection.hpp"
 #include "../tcp_server/TcpConnection.hpp"
+#include "../utils/Logger.hpp"
+#include "../utils/Hashing.hpp"
 
-rest::Response rest::Registration::handleRequest(const server::Connection &clientConnection, const rest::Request &request)
+std::shared_ptr<rest::Response> rest::Registration::handleRequest(const server::Connection &clientConnection, const rest::Request &request)
 {
 
     if(!decryptBody(clientConnection,request))
@@ -13,21 +16,42 @@ rest::Response rest::Registration::handleRequest(const server::Connection &clien
         return IRoute::createResponse(boost::beast::http::status::bad_request,"Failed to decrypt request body!",StatusCodes::FAILED_TO_DECRYPT_BODY,request);
     }
 
-    Response response;
+    std::shared_ptr<Response> response = std::make_shared<Response>();
     auto userData = parseBody(clientConnection,response,request);
-    if(userData.id < 0)
+    if(!response->body().empty())
     {
         return response;
     }
 
-    return rest::Response();
+    if(checkIfEmailExistsInDatabase(userData.email,response,request))
+    {
+        return IRoute::createResponse(boost::beast::http::status::ok,"User with this login exist!",StatusCodes::USER_WITH_EMAIL_EXIST,request);
+    }
+
+    if(!response->body().empty())
+    {
+        return response;
+    }
+
+    if(checkIfLoginExistsInDatabase(userData.login,response,request))
+    {
+        return IRoute::createResponse(boost::beast::http::status::ok,"User with this email exist!",StatusCodes::USER_WITH_LOGIN_EXIST,request);
+    }
+
+    if(!response->body().empty())
+    {
+        return response;
+    }
+
+    writeRegistrationDataToDatabase(userData,response,request);
+    return response;
 }
 
 bool rest::Registration::decryptBody(const server::Connection &clientConnection, const rest::Request &request)
 {
     return clientConnection->decrypt(request.body(),decryptedBody);
 }
-rest::Registration::UserRegistrationData rest::Registration::parseBody(const server::Connection &clientConnection,Response &response,const Request &request)
+rest::Registration::UserRegistrationData rest::Registration::parseBody(const server::Connection &clientConnection,std::shared_ptr<Response> &response,const Request &request)
 {
     UserRegistrationData userRegistrationData;
     try
@@ -45,3 +69,60 @@ rest::Registration::UserRegistrationData rest::Registration::parseBody(const ser
     }
     return userRegistrationData;
 }
+
+bool rest::Registration::checkIfLoginExistsInDatabase(const std::string& login,std::shared_ptr<Response> &response,const Request &request)
+{
+    pqxx::result result;
+    try
+    {
+        auto connection= db::Connection::connect();
+        auto work = connection->getWork();
+        result = work->exec_params("SELECT * FROM users WHERE login=$1",login);
+    }
+    catch (const std::exception &ex)
+    {
+        BOOST_LOG_TRIVIAL(error) << ex.what();
+        response = IRoute::createResponse(boost::beast::http::status::bad_request,ex.what(),StatusCodes::DATABASE_NOT_RESPONDING,request);
+    }
+
+    return !result.empty();
+}
+
+bool rest::Registration::checkIfEmailExistsInDatabase(const std::string &email,std::shared_ptr<Response> &response,const Request &request)
+{
+    pqxx::result result;
+    try
+    {
+        auto connection= db::Connection::connect();
+        auto work = connection->getWork();
+        result = work->exec_params("SELECT * FROM users WHERE email=$1",email);
+    }
+    catch (const std::exception &ex)
+    {
+        BOOST_LOG_TRIVIAL(error) << ex.what();
+        response = IRoute::createResponse(boost::beast::http::status::bad_request,ex.what(),StatusCodes::DATABASE_NOT_RESPONDING,request);
+    }
+
+    return !result.empty();
+}
+
+void rest::Registration::writeRegistrationDataToDatabase(const rest::Registration::UserRegistrationData &userRegistrationData, std::shared_ptr<Response> &response, const rest::Request &request)
+{
+    try
+    {
+        auto connection= db::Connection::connect();
+        auto work = connection->getWork();
+        work->exec_params("INSERT INTO users (user_id, email, login, password) VALUES ($1, $2, $3, $4); ",userRegistrationData.id,userRegistrationData.email,userRegistrationData.login, utils::hashPassword(userRegistrationData.password));
+        work->commit();
+    }
+    catch (const std::exception &ex)
+    {
+        BOOST_LOG_TRIVIAL(error) << ex.what();
+        response = IRoute::createResponse(boost::beast::http::status::bad_request,ex.what(),StatusCodes::DATABASE_NOT_RESPONDING,request);
+    }
+    if(response->body().empty())
+    {
+        response = IRoute::createResponse(boost::beast::http::status::ok,"Registration successful.",StatusCodes::OK,request);
+    }
+}
+
--- server/src/http/Registration.hpp
+++ server/src/http/Registration.hpp
@@ -17,12 +17,16 @@ namespace rest
             std::string email;
             std::string password;
         };
+
         std::string decryptedBody;
         bool decryptBody(const server::Connection &clientConnection, const Request &request);
-        UserRegistrationData parseBody(const server::Connection &clientConnection,Response &response,const Request &request);
+        UserRegistrationData parseBody(const server::Connection &clientConnection,std::shared_ptr<Response> &response,const Request &request);
+        bool checkIfLoginExistsInDatabase(const std::string& login,std::shared_ptr<Response> &response,const Request &request);
+        bool checkIfEmailExistsInDatabase(const std::string& email,std::shared_ptr<Response> &response,const Request &request);
+        void writeRegistrationDataToDatabase(const UserRegistrationData &userRegistrationData,std::shared_ptr<Response> &response,const Request &request);
 
     public:
-        Response handleRequest(const server::Connection &clientConnection, const Request &request) override;
+        std::shared_ptr<Response> handleRequest(const server::Connection &clientConnection, const Request &request) override;
     };
 }
 
